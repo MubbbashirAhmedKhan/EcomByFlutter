cdb0491ba3678f45bcb1d0bd01980bc7237f4f65
MubbbashirAhmedKhan <76056522+MubbbashirAhmedKhan@users.noreply.github.com>
2021-06-28 00:15:46 -0700

    Update main.dart

--- main.dart
+++ main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+
 import 'home.dart';
+
+
 
 void main() {
   runApp(MyApp());
@@ -9,10 +12,38 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        home: Scaffold(
-        
-          body: Home(),
-        ));
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Home(),
+        appBar: AppBar(
+          title: Text("Ecom App"),
+          actions: <Widget>[
+            IconButton(
+              icon: Icon(Icons.comment),
+              tooltip: 'Comment Icon',
+              onPressed: () {},
+            ), //IconButton
+            IconButton(
+              icon: Icon(Icons.settings),
+              tooltip: 'Setting Icon',
+              onPressed: () {},
+            ), //IconButton
+          ], //<Widget>[]
+          backgroundColor: Colors.blue,
+          // elevation: 50.0,
+          leading: IconButton(
+            icon: Icon(Icons.menu),
+            tooltip: 'Menu Icon',
+            onPressed: () {},
+          ), //IconButton
+          // brightness: Brightness.dark,
+        ), //AppBar
+        // body: const Center(
+        //   child: Text(
+        //
+        //     style: TextStyle(fontSize: 24),
+        //   ), //Text
+      ),
+    );
   }
 }
